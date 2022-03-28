7815a03f77a074071eb7d1c83075ebf465e19ced
Chế Vũ Gia Hy <cvghy116@gmail.com>
2022-03-28 18:08:30 +0700

    ADd sample publish banners from file

--- publisher/src/main.rs
+++ publisher/src/main.rs
@@ -1,12 +1,14 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 use paho_mqtt::AsyncClient;
-use tokio::time;
 
 #[tokio::main]
 async fn main() {
     let broker_url =
         env::var("MQTT_BROKER_URL").unwrap_or_else(|_| "tcp://localhost:1883".to_string());
+    let broker_topic = env::var("MQTT_BROKER_TOPIC").unwrap_or_else(|_| "testTopic".to_string());
     let client = AsyncClient::new(broker_url).expect("Error creating the client");
     println!("Connecting to the MQTT server");
     let token = client.connect(None).await;
@@ -14,18 +16,21 @@ async fn main() {
         println!("Error: {}", err);
     }
 
-    let mut count = 0;
-    loop {
-        println!("Publishing a message on the topic 'testTopic'");
-        let msg = paho_mqtt::Message::new("testTopic", "Hello Rust MQTT world!", paho_mqtt::QOS_1);
-        if let Err(err) = client.publish(msg).await {
-            println!("Publishing error: {}", err);
+    let mut lines = Vec::<String>::with_capacity(1000);
+    for line in BufReader::new(File::open("banner.json").expect("Error opening file")).lines() {
+        lines.push(line.unwrap());
+    }
+    println!("Publishing messages on the topic `{}`", broker_topic);
+    let mut cond = true;
+    while cond {
+        for line in &lines {
+            let msg = paho_mqtt::Message::new(&broker_topic, line.as_bytes(), paho_mqtt::QOS_1);
+            if let Err(err) = client.publish(msg).await {
+                println!("Publishing error: {}", err);
+                cond = false;
+                break;
+            }
         }
-        time::sleep(time::Duration::from_secs(1)).await;
-        if count == 100 {
-            break;
-        }
-        count += 1;
     }
 
     // Disconnect from the broker
